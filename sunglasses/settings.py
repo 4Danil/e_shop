9da9a56bac365d0511e2f3307ec08a18b14ac86d
Danil1307 <danil20001313@gmail.com>
2024-06-05 14:41:16 +0400

    STATIC_ROOT

--- sunglasses/settings.py
+++ sunglasses/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%mhv&gg-5v6%f^y)xp=ke5shv)0k!w$6)zot2sr!kn2vwmtnm^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['4Danil.pythonanywhere.com']
 
 
 # Application definition
@@ -125,13 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/home/4Danil/e_shop/static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / '/home/4Danil/e_shop/media'
 
 
 # Default primary key field type
